548150ee70eafca9953b57cfc2c8363c8df48125
Ejimofor Nwoye <eknwoye@users.noreply.github.com>
2025-05-26 00:12:32 +0100

    Update ukmpprofile2_json.py

--- ukmpprofile2_json.py
+++ ukmpprofile2_json.py
@@ -1,4 +1,4 @@
-/usr/bin/env python3
+#!/usr/bin/env python3
 
 # DISCLAIMER: The Application code script and tool is intended to facilitate research, by authorised and approved parties, pursuant to the ideals of libertarian democracy in the UK, by Campaign Lab membership. Content subject-matter and results can be deemed sensitive and thus confidential. Therefore illicit and authorisation for any other use, outside these terms, is hereby not implied pursuant to requisite UK Data Protection legislation and the wider GDPR enactments within the EU.
 
